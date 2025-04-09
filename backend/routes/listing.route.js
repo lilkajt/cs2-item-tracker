69386c5b8b5e2b05aa6e222cab38c8ac460a2490
lilkajt <87129505+lilkajt@users.noreply.github.com>
2025-04-09 20:49:55 +0200

    delete item added

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { verifyUser } from '../utils/verifyUser.js';
-import { createItem } from '../controllers/listing.controller.js';
+import { createItem, deleteItem } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyUser, createItem);
+router.delete('/delete/:id', verifyUser, deleteItem);
+
 export default router;
