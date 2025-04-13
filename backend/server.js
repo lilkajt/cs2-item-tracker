056926502ddeac7d700fdcd5df734f4a796f43cd
lilkajt <87129505+lilkajt@users.noreply.github.com>
2025-04-13 21:54:49 +0200

    added error messages

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,21 @@ app.use('/api/user', userRouter);
 app.use('/api/item', listingRouter);
 app.use( (err,req, res, next) => {
     const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
+    let message = "Whoops, something broke! Try again in a minute.";
+    if (err.name === "TypeError") {
+        message = "Oops! Looks like we didn't get your input right. Please double-check and try again!";
+    } else if (err.name === "ValidationError") {
+        message = "Hmm, we couldn't make sense of some of the data. Give it another go, making sure everything is in order!";
+    } else if (err.statusCode && err.statusCode < 500) {
+        message = err.message;
+    }
+    if (statusCode >= 500){
+        console.log('Server error: ', err);
+    }
     return res.status(statusCode).json({
         success: false,
-        statusCode,
-        message
+        statusCode: statusCode,
+        message: message
     });
 });
 
