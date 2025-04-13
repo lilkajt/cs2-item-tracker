e888331a9b38b0ca31f4a48d0a70912b341634d0
lilkajt <87129505+lilkajt@users.noreply.github.com>
2025-04-13 18:56:09 +0200

    app path update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Dashboard from './pages/Dashboard'
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+          <Route path='/about' element={<About/>} />
+          <Route element={<PrivateRoute />} >
+            <Route path='/dashboard' element={ <Dashboard />} />
+            <Route path='/profile' element={ <Profile />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
   )
