015d07467a8790fdd22c89722998dde1e0466896
lilkajt <87129505+lilkajt@users.noreply.github.com>
2025-04-17 13:39:04 +0200

    header complete

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,28 +1,39 @@
 import Button from './Button'
 import { FiUser } from "react-icons/fi";
+import useAuthStore from '@/store/useAuthStore';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 function Header() {
-    let loggedIn = false;
-    let OnSignInPage = false;
-    // if logged in show dashboard header == cs2 tracker and user icon
-    // if not logged in show only sign in
-    // if click sign in display only logo no button on login/signup page
+    const {isLoggedIn} = useAuthStore();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const isSignInPage = location.pathname === '/sign-in';
+    const handleSignIn = () => {
+        navigate('/sign-in');
+    };
   return (
     <>
     <div className='w-full h-32'>
         <div className='w-full py-8 px-8 inline-flex flex-row items-center justify-between'>
-            <div className="w-auto h-6 text-center justify-center text-beige-100 text-3xl font-bold font-display2 cursor-pointer">FinTrack</div>
-            { loggedIn && OnSignInPage ? (
+            <Link to={'/'}>
+                <div className="w-auto h-6 text-center text-beige-100 text-3xl font-bold font-display2 cursor-pointer">FinTrack</div>
+            </Link>
+            { isLoggedIn && (
                 <>
-                    <div className='w-auto h-[40px] cursor-pointer text-beige-100 hover:text-beige-200 flex items-center'>
-                        <div className='text-inherit font-display2 mt-2'>cs2 tracker</div>
-                    </div>
-                    <div className='h-[40px] w-[52px] flex items-center justify-center text-beige-100 hover:text-beige-200 cursor-pointer'>
-                        <FiUser size={32}/>
-                    </div>
+                    <Link to={'/dashboard'}>
+                        <div className='w-auto h-[40px] cursor-pointer text-beige-100 hover:text-beige-200 flex items-center'>
+                            <div className='text-inherit font-display2 mt-2'>cs2 tracker</div>
+                        </div>
+                    </Link>
+                    <Link to={'/profile'}>
+                        <div className='h-[40px] w-[52px] flex items-center justify-center text-beige-100 hover:text-beige-200 cursor-pointer'>
+                            <FiUser size={32}/>
+                        </div>
+                    </Link>
                 </>
-            ) : (
-                <Button color='green'>Sign In</Button>
+            )}
+            { !isLoggedIn && !isSignInPage && (
+                <Button onClick={handleSignIn} color='green'>Sign In</Button>
             )}
         </div>
     </div>
