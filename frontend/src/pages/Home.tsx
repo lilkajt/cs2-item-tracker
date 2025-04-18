8935f2e15e196d23388b10a8bd855975bcf80db8
lilkajt <87129505+lilkajt@users.noreply.github.com>
2025-04-18 20:43:51 +0200

    styles update

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import CenterSquare from "@/components/CenterSquare"
+import Title from "@/components/Title"
 
 function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-17">
         <CenterSquare/>
+        <Title color="beige">Our Features</Title>
     </div>
   )
 }
