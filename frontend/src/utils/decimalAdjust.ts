18a7cff4c9d8abea87a4de8d066f1ef8f88d9687
lilkajt <87129505+lilkajt@users.noreply.github.com>
2025-04-24 12:35:52 +0200

    add decimalAdjust function

---
+++ frontend/src/utils/decimalAdjust.ts
@@ -0,0 +1,22 @@
+function decimalAdjust(type:string, value:number, exp:number) {
+  type = String(type);
+  if (!["round", "floor", "ceil"].includes(type)) {
+    throw new TypeError(
+      "The type of decimal adjustment must be one of 'round', 'floor', or 'ceil'.",
+    );
+  }
+  exp = Number(exp);
+  value = Number(value);
+  if (exp % 1 !== 0 || Number.isNaN(value)) {
+    return NaN;
+  } else if (exp === 0) {
+    return Math[type](value);
+  }
+  const [magnitude, exponent = 0] = value.toString().split("e");
+  const adjustedValue = Math[type](`${magnitude}e${exponent - exp}`);
+  // Shift back
+  const [newMagnitude, newExponent = 0] = adjustedValue.toString().split("e");
+  return Number(`${newMagnitude}e${+newExponent + exp}`);
+};
+
+export const round10 = (value:number, exp:number) => decimalAdjust("round", value, exp);
