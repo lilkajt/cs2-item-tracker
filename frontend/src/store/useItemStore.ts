cd1fd605d43d6bb14875a1abe8a739b23d63e0f8
lilkajt <87129505+lilkajt@users.noreply.github.com>
2025-04-26 11:09:58 +0200

    add types + fetch stats func

--- frontend/src/store/useItemStore.ts
+++ frontend/src/store/useItemStore.ts
@@ -25,13 +25,32 @@ export type ServerResponse = {
   message: string
 }
 
+export type ItemStats = {
+  averageROI: string,
+  averagePrices: {
+    buy: string,
+    sell: string
+  },
+  highestProfitItem:{
+    itemName: string,
+    profit: string
+  } | null,
+  itemsPurchasedThisMonth: number,
+  monthlyData: Array<{
+    name: string,
+    value: number
+  }>
+}
+
 type ItemState = {
   items: Item[]
   loading: boolean
   error: string | null
+  stats: ItemStats | null
   pagination: Pagination
   serverResponse: ServerResponse | null
   fetchItems: (page?: number, limit?: number) => Promise<void>
+  fetchStats: () => Promise<void>
   updateItem: (id: string, updatedItem: Partial<Item>) => Promise<void>
   deleteItem: (id: string) => Promise<void>
   clearServerResponse: () => void
@@ -43,6 +62,7 @@ const useItemStore = create<ItemState>((set) => ({
   items: [],
   loading: false,
   error: null,
+  stats: null,
   serverResponse: null,
   pagination: {
     totalItems: 0,
@@ -63,7 +83,21 @@ const useItemStore = create<ItemState>((set) => ({
       set({ loading: false, error: error.response.data.message })
     });
   },
-  
+
+  fetchStats: async () => {
+    set({ loading: true, error: null });
+    await axios.get(`${API_URL}/stats`)
+      .then(response => {
+        set({ stats: response.data.stats, loading: false });
+      })
+      .catch(error => {
+        set({ 
+          loading: false, 
+          error: error.response?.data?.message || 'Failed to load statistics'
+        });
+      });
+  },
+
   updateItem: async (id, updatedItem) => {
     set({ loading: true, error: null, serverResponse: null })
     await axios.put(`${API_URL}/update/${id}`, updatedItem)
