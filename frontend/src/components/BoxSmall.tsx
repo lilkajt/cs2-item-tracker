6033b09df831846a051efb6b6184b44b94613fbe
lilkajt <87129505+lilkajt@users.noreply.github.com>
2025-05-07 22:26:36 +0200

    import delete

--- frontend/src/components/BoxSmall.tsx
+++ frontend/src/components/BoxSmall.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function BoxSmall() {
   return (
     <div>BoxSmall</div>
--- frontend/src/components/ColumnElement.tsx
+++ frontend/src/components/ColumnElement.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function ColumnElement() {
   return (
     <div>ColumnElement</div>
