2349978db635df7accbd682cb78c682c09dbb1b7
lilkajt <87129505+lilkajt@users.noreply.github.com>
2025-05-07 18:02:54 +0200

    decimalAdjust change

--- frontend/src/utils/decimalAdjust.ts
+++ frontend/src/utils/decimalAdjust.ts
@@ -1,22 +1,20 @@
-function decimalAdjust(type:string, value:number, exp:number) {
-  type = String(type);
-  if (!["round", "floor", "ceil"].includes(type)) {
-    throw new TypeError(
-      "The type of decimal adjustment must be one of 'round', 'floor', or 'ceil'.",
-    );
+export function round10(value: number, exp: number): number {
+  if (!exp) {
+    return Math.round(value);
   }
-  exp = Number(exp);
+  
   value = Number(value);
-  if (exp % 1 !== 0 || Number.isNaN(value)) {
+  exp = Number(exp);
+  
+  if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
     return NaN;
-  } else if (exp === 0) {
-    return Math[type](value);
   }
-  const [magnitude, exponent = 0] = value.toString().split("e");
-  const adjustedValue = Math[type](`${magnitude}e${exponent - exp}`);
-  // Shift back
-  const [newMagnitude, newExponent = 0] = adjustedValue.toString().split("e");
-  return Number(`${newMagnitude}e${+newExponent + exp}`);
-};
 
-export const round10 = (value:number, exp:number) => decimalAdjust("round", value, exp);
+  // Shift
+  const valueStr = value.toString().split('e');
+  value = Math.round(Number(valueStr[0] + 'e' + (valueStr[1] ? (+valueStr[1] - exp) : -exp)));
+  
+  // Shift back
+  const valStr = value.toString().split('e');
+  return Number(valStr[0] + 'e' + (valStr[1] ? (+valStr[1] + exp) : exp));
+}
