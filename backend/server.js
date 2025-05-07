a30905ebe5683162b84f3c21b181c8e6097b71e0
lilkajt <87129505+lilkajt@users.noreply.github.com>
2025-05-07 18:16:47 +0200

    production build

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.route.js';
 import userRouter from './routes/user.route.js';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
+
+const __dirname = path.resolve();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,6 +15,14 @@ app.use(cookieParser());
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/items', listingRouter);
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get('/*\w', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+};
+
 app.use( (err,req, res, next) => {
     const statusCode = err.statusCode || 500;
     let message = "Whoops, something broke! Try again in a minute.";
